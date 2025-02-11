d11eeaca8446a7a023755cf886b778e8861c4102
Muhammad <muhammadkodirov089@gmail.com>
2025-02-11 13:18:45 +0100

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,137 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Github, Linkedin, Mail, Twitter } from "lucide-react"
+import Link from "next/link"
+import ContactForm from "./components/contact-form"
+import ProjectCard from "./components/project-card"
+import TechStack from "./components/tech-stack"
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen bg-background">
+      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 items-center">
+          <div className="mr-4 hidden md:flex">
+            <Link className="mr-6 flex items-center space-x-2" href="/">
+              <span className="hidden font-bold sm:inline-block">MK Dev.</span>
+            </Link>
+            <nav className="flex space-x-6 text-sm font-medium items-center">
+              <Link href="#about" className="transition-colors hover:text-foreground/80">
+                Über
+              </Link>
+              <Link href="#projects" className="transition-colors hover:text-foreground/80">
+                Projekte
+              </Link>
+              <Link href="#contact" className="transition-colors hover:text-foreground/80">
+                Kontakt
+              </Link>
+            </nav>
+          </div>
+          {/* <Link className="mx-auto lg:mx-0 lg:ml-auto " target="_blank" href="/Lebenslauf_.pdf">
+            <Button variant="outline">
+              Portfolio als PDF
+            </Button>
+          </Link> */}
         </div>
+      </header>
+
+      <main className="container px-4 md:px-6">
+        <section id="about" className="py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                  Frontend Developer
+                </h1>
+                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                  Ich entwickle moderne und ansprechende digitale Erlebnisse mit den neuesten Technologien. Mein Fokus liegt auf eleganten Frontend-Lösungen für komplexe Herausforderungen – leistungsstark, benutzerfreundlich und ästhetisch überzeugend.
+                </p>
+              </div>
+              <div className="space-x-4">
+                <Link href="https://github.com/muhakodirov" target="_blank">
+                  <Button variant="outline" size="icon">
+                    <Github className="h-4 w-4" />
+                    <span className="sr-only">GitHub</span>
+                  </Button>
+                </Link>
+
+                <Link href="mailto:muhammadkadirov089@gmail.com">
+                  <Button variant="outline" size="icon">
+                    <Mail className="h-4 w-4" />
+                    <span className="sr-only">Email</span>
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="projects" className="py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">Projekte</h2>
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+              <ProjectCard
+                // Photo by Maksim Larin on Unsplash
+                title="Sneakers-Store: Online Shop"
+                description="Eine moderne E-Commerce-Webanwendung mit benutzerdefinierter Filterung, Warenkorbfunktion und Responsive Design."
+                image="/sneakers-store.webp"
+                link="https://github.com/muhakodirov/Sneakers-store"
+                tags={["Next.js", "TypeScript", "Tailwind", "Context API", "MongoDB"]}
+              />
+              <ProjectCard
+                // Photo by Maxim Hopman on Unsplash
+                title="Crypto Tracker/Converter - Only Desktop"
+                description="Eine Anwendung zur Verfolgung und Konvertierung von Kryptowährungen. Implementiert Echtzeit-API-Daten und benutzerfreundliche UI."
+                image="/crypto.webp"
+                link="https://github.com/muhakodirov/cryptomaster"
+                tags={["React", "Module CSS", "Binance API", "Redux", "React Router"]}
+              />
+              <ProjectCard
+                // Photo by TRNAVA UNIVERSITY on Unsplash
+                title="Book Library"
+                description="Eine einfache Webanwendung, die es Nutzern ermöglicht, neue Bücher hinzuzufügen und als Favorite zu speichern."
+                image="/book-library.webp"
+                link="https://github.com/muhakodirov/Book-Library-App"
+                tags={["React", "CSS", "Redux"]}
+              />
+              <ProjectCard
+                // Photo by MARIOLA GROBELSKA on Unsplash
+                title="InRecipes - Recipe Blog"
+                description="Eine Plattform zum Durchsuchen, Speichern und Hinzufügen von Rezepten (nach Anmeldung), mit schneller Datenabfrage und responsivem Design"
+                image="/recipe.webp"
+                link="https://github.com/muhakodirov/recipes-app"
+                tags={["Next.js", "TypeScript", "Tailwind", "Shadcn", "React Query", "Context API", "MongoDB",]}
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">
+              Tech Stack
+            </h2>
+            <TechStack />
+          </div>
+        </section>
+
+        <section id="contact" className="py-12 md:py-24 lg:py-32">
+          <div className="container px-4 md:px-6">
+            <div className="mx-auto max-w-2xl">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-12 text-center">
+                Kontaktieren Sie mich
+              </h2>
+              <ContactForm />
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="border-t">
+        <div className="container flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
+          <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 MK Dev. All rights reserved.</p>
+        </div>
       </footer>
     </div>
-  );
+  )
 }
+
